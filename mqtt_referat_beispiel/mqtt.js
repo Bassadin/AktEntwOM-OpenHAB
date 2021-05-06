20e99fc14fc56baa0d5c3bf9f9d6c284fec66682
Bastian Hodapp <bastianhodapp@gmail.com>
2021-05-06 16:21:33 +0200

    use cloud-shiftr for better visual demo, clean up code a bit

--- mqtt_referat_beispiel/mqtt.js
+++ mqtt_referat_beispiel/mqtt.js
@@ -1,15 +1,21 @@
 let mqtt = require('mqtt')
-let client = mqtt.connect('mqtt://public:public@public.cloud.shiftr.io', { clientId: "bastian_hfu" });
+let client = mqtt.connect('mqtt://hfu-dm:1234@hfu-dm.cloud.shiftr.io', { clientId: "bastian_hfu" });
 
 client.on('connect', () => {
-    client.subscribe('hfu/dm/test', { qos: 2 });
-    client.publish('hfu/dm/test', 'Hallo mqtt', { qos: 1, retain: true });
+    client.subscribe(
+        'hfu/dm/test',
+        { qos: 2 }
+    );
+    client.publish(
+        'hfu/dm/test',
+        'Hallo mqtt',
+        { qos: 1, retain: true }
+    );
 })
 
 //Handle subscriptions
 client.on('message', (topic, message, packet) => {
     console.log("Topic:", topic);
     console.log("Message:", message.toString()); //.toString weil Buffer
-    console.log("Packet:", packet);
     // client.end()
 })
