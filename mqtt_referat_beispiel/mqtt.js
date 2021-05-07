01e01ec9e5b66b9d2c08f6db0f17eca5bb1a82b2
Bastian Hodapp <bastianhodapp@gmail.com>
2021-05-07 16:47:19 +0200

    Change formatting back to one line

--- mqtt_referat_beispiel/mqtt.js
+++ mqtt_referat_beispiel/mqtt.js
@@ -2,15 +2,8 @@ let mqtt = require('mqtt')
 let client = mqtt.connect('mqtt://hfu-dm:1234@hfu-dm.cloud.shiftr.io', { clientId: "bastian_hfu" });
 
 client.on('connect', () => {
-    client.subscribe(
-        'hfu/dm/test',
-        { qos: 1 }
-    );
-    client.publish(
-        'hfu/dm/test',
-        'Hallo mqtt',
-        { qos: 1, retain: true }
-    );
+    client.subscribe('hfu/dm/test', { qos: 1 });
+    client.publish('hfu/dm/test', 'Hallo mqtt', { qos: 1, retain: true });
 })
 
 //Handle subscriptions
