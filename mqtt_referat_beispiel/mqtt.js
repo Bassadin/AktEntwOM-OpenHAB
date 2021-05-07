845e0f43fcbcbcabfca0fb1d3ad4ae5ec003f8a6
Bastian Hodapp <bastianhodapp@gmail.com>
2021-05-07 16:47:41 +0200

    More semicolons

--- mqtt_referat_beispiel/mqtt.js
+++ mqtt_referat_beispiel/mqtt.js
@@ -1,14 +1,14 @@
-let mqtt = require('mqtt')
+let mqtt = require('mqtt');
 let client = mqtt.connect('mqtt://hfu-dm:1234@hfu-dm.cloud.shiftr.io', { clientId: "bastian_hfu" });
 
 client.on('connect', () => {
     client.subscribe('hfu/dm/test', { qos: 1 });
     client.publish('hfu/dm/test', 'Hallo mqtt', { qos: 1, retain: true });
-})
+});
 
 //Handle subscriptions
 client.on('message', (topic, message, packet) => {
     console.log("Topic:", topic);
     console.log("Message:", message.toString()); //.toString weil Buffer
     // client.end()
-})
+});
